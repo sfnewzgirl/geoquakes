e23dcd2d628e4f60dabcbd0cf5b44aa3e3418ba4
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-24 15:53:17 -0700

    pair programming geoquakes

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,33 @@ var template;
 
 $(document).on("ready", function() {
 
+  $.ajax ({
+    method: 'GET',
+    url: weekly_quakes_endpoint,
+    datatype: 'json',
+    success: onSuccess,
+  })
 
 });
+
+  function onSuccess(json) {
+    //SUCCESS calls first earthquake
+    //console.log(json.features[0]);
+
+    var jsonData = json;
+    console.log("heres our data:", jsonData);
+
+    var source = $('#recent_earthquakes').html();
+      console.log(source);
+
+    var quakeTemplate = Handlebars.compile(source);
+
+    var quakeHtml = quakeTemplate({
+      magnitude: jsonData.features[0].properties.mag,
+      quake_location: jsonData.features[0].properties.place,
+    });
+    console.log(quakeHtml);
+
+    $("quake_list").append(quakeHtml);
+
+  };
