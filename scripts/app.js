a127c11821ebc9083ff60678eee2813b32940577
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-24 22:34:42 -0700

    adds map and baseline google api code in js

--- scripts/app.js
+++ scripts/app.js
@@ -34,3 +34,51 @@ $(document).on("ready", function() {
     $("#quake_list").append(quakeHtml);
 
   };
+
+  window.eqfeed_callback = function(results) {
+          for (var i = 0; i < results.features.length; i++) {
+            var coords = results.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[1],coords[0]);
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map
+            });
+          }
+        }
+
+  function initialize() {
+
+        map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+
+        // (In this example we use a locally stored copy instead.)
+        // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+        script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
+        document.getElementsByTagName('head')[0].appendChild(script);
+
+        map.data.setStyle(function(feature) {
+          var magnitude = feature.getProperty('mag');
+          return {
+            icon: getCircle(magnitude)
+          };
+        });
+      }
+
+      function getCircle(magnitude) {
+        var circle = {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: 'red',
+          fillOpacity: .2,
+          scale: Math.pow(2, magnitude) / 2,
+          strokeColor: 'white',
+          strokeWeight: .5
+        };
+        return circle;
+      }
+
+      function eqfeed_callback(results) {
+        map.data.addGeoJson(results);
+      }
